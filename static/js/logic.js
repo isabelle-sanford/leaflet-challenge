e0e8b36ad935e3dee79b41c631878c78a135a779
Isabelle Sanford <isabellesanford@gmail.com>
2021-01-31 15:16:13 -0800

    substitute appropriate variables into template

---
+++ static/js/logic.js
@@ -0,0 +1,65 @@
+
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+
+var centerCoords = [40.73, -74.0059];
+
+function createMap(earthquakes) {
+
+    // Create the tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+  
+    // Create a baseMaps object to hold the lightmap layer
+    var baseMaps = {
+      "Light Map": lightmap
+    };
+  
+    // Create an overlayMaps object to hold the bikeStations layer
+    var overlayMaps = {
+      "Earthquakes": earthquakes
+    };
+  
+    // Create the map object with options
+    var map = L.map("map-id", {
+      center: centerCoords,
+      zoom: 12,
+      layers: [lightmap, earthquakes]
+    });
+  
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+}
+  
+function createMarkers(response) {
+    // Pull the "stations" property off of response.data
+    var features = response.features;
+
+    // Initialize an array to hold markers
+    var quakeMarkers = [];
+
+    // Loop through the array
+    for (var i = 0; i < features.length; i++) {
+        var quake = features[index];
+
+        // For each quake, create a marker and bind a popup with the station's name
+        var quakeMarker = L.marker([quake.geometry.coordinates[0], quake.geometry.coordinates[1]])
+        .bindPopup("<h3>" + quake.properties.mag + "</h3>");
+
+        // Add the marker to the array
+        quakeMarkers.push(quakeMarker);
+    }
+
+    // Create a layer group made from the bike markers array, pass it into the createMap function
+    createMap(L.layerGroup(quakeMarkers));
+}
+  
+  
+  // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
+  d3.json(url, createMarkers);
+  
